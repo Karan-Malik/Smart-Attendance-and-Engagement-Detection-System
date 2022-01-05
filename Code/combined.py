131ceb39a2a45f25dd30d88e95bd9851ca4c3087
Karan-Malik <karanmalik2000@gmail.com>
2022-01-05 15:36:13 +0530

    First commit

---
+++ Code/combined.py
@@ -0,0 +1,283 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  8 18:59:42 2021
+
+@author: karan
+"""
+
+import datetime
+import os
+import time
+import cv2
+import pandas as pd
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import playsound
+import argparse
+import imutils
+import dlib
+
+
+
+def recognize_attendence():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()  
+    recognizer.read("TrainingImageLabel"+os.sep+"Trainner.yml")
+    harcascadePath = "haarcascade_default.xml"
+    faceCascade = cv2.CascadeClassifier(harcascadePath)
+    df = pd.read_csv("StudentDetails"+os.sep+"StudentDetails.csv",header=None)
+    df.columns=['Id','Name']
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    col_names = ['Id', 'Name', 'Date', 'Time']
+    attendance = pd.DataFrame(columns=col_names)
+
+    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cam.set(3, 640) 
+    cam.set(4, 480) 
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+
+    while True:
+        ret, im = cam.read()
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5,
+                minSize = (int(minW), int(minH)),flags = cv2.CASCADE_SCALE_IMAGE)
+        for(x, y, w, h) in faces:
+            cv2.rectangle(im, (x, y), (x+w, y+h), (10, 159, 255), 2)
+            Id, conf = recognizer.predict(gray[y:y+h, x:x+w])
+            if conf < 100:
+                #print(df)
+                aa = df.loc[df['Id'] == Id]['Name'].values
+                confstr = "  {0}%".format(round(100 - conf))
+                tt = str(Id)+"-"+aa
+            else:
+                Id = '  Unknown  '
+                tt = str(Id)
+                confstr = "  {0}%".format(round(100 - conf))
+
+            if (conf) > 55:
+                ts = time.time()
+                date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                aa = str(aa)[2:-2]
+                attendance.loc[len(attendance)] = [Id, aa, date, timeStamp]
+
+            tt = str(tt)[2:-2]
+            if(100-conf) > 67:
+                tt = tt + " [Pass]"
+                cv2.putText(im, str(tt), (x+5,y-5), font, 1, (255, 255, 255), 2)
+            else:
+                cv2.putText(im, str(tt), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+
+            if (100-conf) > 67:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font,1, (0, 255, 0),1 )
+            elif (100-conf) > 50:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 255, 255), 1)
+            else:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 0, 255), 1)
+
+
+        attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
+        
+        cv2.imshow('Attendance', im)
+        if (cv2.waitKey(1) == ord('q')):
+            break
+   
+    print(attendance)
+    ts = time.time()
+    date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+    Hour, Minute, Second = timeStamp.split(":")
+    fileName = "Attendance"+os.sep+"Attendance_"+date+"_"+Hour+"-"+Minute+"-"+Second+".csv"
+    attendance.to_csv(fileName, index=False)
+    print("Attendance Successful")
+    cam.release()
+    cv2.destroyAllWindows()
+    
+
+
+
+def eye_aspect_ratio(eye):
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+	C = dist.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
+ 
+
+def detect_drowsy():
+    
+    EYE_AR_THRESH = 0.3
+    EYE_AR_CONSEC_FRAMES = 48
+    
+    COUNTER = 0
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    
+    vs = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    time.sleep(1.0)
+    
+    while True:
+    	_,frame = vs.read()
+    	frame = imutils.resize(frame, width=450)
+    	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
+    	rects = detector(gray, 0)
+    
+    	for rect in rects:
+    		shape = predictor(gray, rect)
+    		shape = face_utils.shape_to_np(shape)
+    		leftEye = shape[lStart:lEnd]
+    		rightEye = shape[rStart:rEnd]
+    		leftEAR = eye_aspect_ratio(leftEye)
+    		rightEAR = eye_aspect_ratio(rightEye)
+    
+    		ear = (leftEAR + rightEAR) / 2.0
+    
+    		leftEyeHull = cv2.convexHull(leftEye)
+    		rightEyeHull = cv2.convexHull(rightEye)
+    		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+    		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+    
+    		if ear < EYE_AR_THRESH:
+    			COUNTER += 1
+    
+    			if COUNTER >= EYE_AR_CONSEC_FRAMES:
+    				cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),
+    					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)    
+    		else:
+    			COUNTER = 0
+
+    		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+    			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+     
+    	cv2.imshow("Frame", frame)
+    	key = cv2.waitKey(1) & 0xFF
+     
+    	if key == ord("q"):
+            break
+    cv2.destroyAllWindows()
+    vs.release()
+
+
+def attendanceAndDrowsy():
+    recognizer = cv2.face.LBPHFaceRecognizer_create()  
+    recognizer.read("TrainingImageLabel"+os.sep+"Trainner.yml")
+    harcascadePath = "haarcascade_default.xml"
+    faceCascade = cv2.CascadeClassifier(harcascadePath)
+    df = pd.read_csv("StudentDetails"+os.sep+"StudentDetails.csv",header=None)
+    df.columns=['Id','Name']
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    col_names = ['Id', 'Name', 'Date', 'Time']
+    attendance = pd.DataFrame(columns=col_names)
+    
+    EYE_AR_THRESH = 0.3
+    EYE_AR_CONSEC_FRAMES = 48
+    
+    COUNTER = 0
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+    
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    
+    cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cam.set(3, 640) 
+    cam.set(4, 480) 
+    minW = 0.1 * cam.get(3)
+    minH = 0.1 * cam.get(4)
+    
+    while True:
+        ret, im = cam.read()
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5,
+                minSize = (int(minW), int(minH)),flags = cv2.CASCADE_SCALE_IMAGE)
+        for(x, y, w, h) in faces:
+            cv2.rectangle(im, (x, y), (x+w, y+h), (10, 159, 255), 2)
+            Id, conf = recognizer.predict(gray[y:y+h, x:x+w])
+            #print(df)
+            aa = df.loc[df['Id'] == Id]['Name'].values
+            confstr = "  {0}%".format(round(100 - conf))
+            tt = str(Id)+"-"+aa
+            
+            if conf>200:
+                Id = '  Unknown  '
+                tt = str(Id)
+                confstr = "  {0}%".format(round(100 - conf))
+
+            if (conf) > 60:
+                ts = time.time()
+                date = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
+                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                aa = str(aa)[2:-2]
+                attendance.loc[len(attendance)] = [Id, aa, date, timeStamp]
+
+            tt = str(tt)[2:-2]
+            if(100-conf) > 67:
+                tt = tt + " [Pass]"
+                cv2.putText(im, str(tt), (x+5,y-5), font, 1, (255, 255, 255), 2)
+            else:
+                cv2.putText(im, str(tt), (x + 5, y - 5), font, 1, (255, 255, 255), 2)
+
+            if (100-conf) > 67:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font,1, (0, 255, 0),1 )
+            elif (100-conf) > 50:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 255, 255), 1)
+            else:
+                cv2.putText(im, str(confstr), (x + 5, y + h - 5), font, 1, (0, 0, 255), 1)
+
+        # frame=im
+        # frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)    
+        rects = detector(gray, 0)
+    
+        for rect in rects:
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+    
+            ear = (leftEAR + rightEAR) / 2.0
+    
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(im, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(im, [rightEyeHull], -1, (0, 255, 0), 1)
+    
+            if ear < EYE_AR_THRESH:
+                COUNTER += 1
+    
+                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                    cv2.putText(im, "DROWSINESS ALERT!", (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)    
+            else:
+                COUNTER = 0
+
+            cv2.putText(im, "EAR: {:.2f}".format(ear), (300, 30),
+    		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        
+        attendance = attendance.drop_duplicates(subset=['Id'], keep='first')
+        
+        cv2.imshow('Attendance', im)
+        if (cv2.waitKey(1) == ord('q')):
+            break
+    print(attendance)
+    ts = time.time()
+    date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+    timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+    Hour, Minute, Second = timeStamp.split(":")
+    fileName = "Attendance"+os.sep+"Attendance_"+date+"_"+Hour+"-"+Minute+"-"+Second+".csv"
+    attendance.to_csv(fileName, index=False)
+    print("Attendance Successful")
+    cam.release()
+    cv2.destroyAllWindows()
+
+#attendanceAndDrowsy()
+        
+        
---
+++ Code/main.py
@@ -0,0 +1,84 @@
+import os
+import capture_image
+import train_image
+import recognize
+import detect_drowsiness
+import combined
+import warnings
+warnings.filterwarnings("ignore")
+
+def title_bar():
+    os.system('cls')
+    print("\n\n\t***** Smart Attendance and Engagement Detection System *****")
+    print("\t\t\tCreated by: Karan Malik and Rigved Alankar")
+
+
+def mainMenu():
+    title_bar()
+    print()
+    print(10 * "*", "MENU", 10 * "*")
+    print("[1] Register")
+    print("[2] Join a Class")
+    print("[3] Train Images")
+    print("[4] Recognize and Attendance")
+    print("[5] Drowsiness Detection")
+    print("[6] Quit")
+    while True:
+        try:
+            choice = int(input("Enter Choice: "))
+            if choice == 1:
+                captureFaces()
+                trainimages()
+                break
+            elif choice == 2:
+                recognizeAndDrowsy()
+                break
+            elif choice == 3:
+                trainImages()
+                break
+            elif choice == 4:
+                recognizeFaces()
+                break
+            elif choice == 5:
+                drowsinessDetect()
+                break
+            elif choice == 6:
+                print("Thank You")
+                break
+            else:
+                print("Invalid Choice. Enter 1-6")
+                mainMenu()
+        except ValueError:
+            print("Invalid Choice. Enter 1-6\n Try Again")
+    exit
+
+
+
+def captureFaces():
+    capture_image.takeImages()
+    print('\nStudent registered\n')
+    
+
+def trainimages():
+    train_image.TrainImages()
+    print('**Model retrained**')
+    key = input("Enter any key to return main menu")
+    mainMenu()
+
+def recognizeFaces():
+    recognize.recognize_attendence()
+    key = input("Enter any key to return main menu")
+    mainMenu()
+
+def drowsinessDetect():
+    detect_drowsiness.detect_drowsy()
+    key = input("Enter any key to return main menu")
+    mainMenu()
+
+def recognizeAndDrowsy():
+    combined.attendanceAndDrowsy()
+    key = input("Enter any key to return main menu")
+    mainMenu()
+
+
+mainMenu()
